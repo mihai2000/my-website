fc2ebd3c9ee34233c2a433a6ad6bb99a7d3a618d
mihai2000 <berari.mihaidan@gmail.com>
2025-05-28 15:09:14 +0300

    up

--- docs/petstore/theme-openapi.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="docusaurus-plugin-openapi-docs" />
-
-/* eslint-disable @typescript-eslint/no-use-before-define */
-
-declare module "@theme/ApiItem/hooks" {
-	export { useTypedDispatch, useTypedSelector };
-}
