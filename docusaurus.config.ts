05dd8a2daa0b092007fee1a32eb18d6df1c370e8
mihai2000 <berari.mihaidan@gmail.com>
2025-05-28 14:38:40 +0300

    gh

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://btest.tboxlabs.com/",
+	url: "https://btest.tboxlabs.com",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/",
