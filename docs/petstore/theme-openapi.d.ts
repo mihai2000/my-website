6aa4cb038244370e366bf43655c67b8c5356d8ec
mihai2000 <berari.mihaidan@gmail.com>
2025-05-28 14:59:57 +0300

    some updates

---
+++ docs/petstore/theme-openapi.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="docusaurus-plugin-openapi-docs" />
+
+/* eslint-disable @typescript-eslint/no-use-before-define */
+
+declare module "@theme/ApiItem/hooks" {
+	export { useTypedDispatch, useTypedSelector };
+}
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,6 +38,7 @@ const config: Config = {
 				docs: {
 					sidebarPath: "./sidebars.ts",
 					docItemComponent: "@theme/ApiItem",
+					// routeBasePath:""
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					//   editUrl:
