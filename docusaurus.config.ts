c146a3cc897507eb79ead4dae80cbe5903a88566
mihai2000 <berari.mihaidan@gmail.com>
2025-05-29 10:48:14 +0300

    blog, git deleted

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,26 +38,8 @@ const config: Config = {
 				docs: {
 					sidebarPath: "./sidebars.ts",
 					docItemComponent: "@theme/ApiItem",
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					//   editUrl:
-					//     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
 				},
-				blog: {
-					showReadingTime: true,
-					feedOptions: {
-						type: ["rss", "atom"],
-						xslt: true,
-					},
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					// editUrl:
-					//   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-					// Useful options to enforce blogging best practices
-					onInlineTags: "warn",
-					onInlineAuthors: "warn",
-					onUntruncatedBlogPosts: "warn",
-				},
+
 				theme: {
 					customCss: "./src/css/custom.css",
 				},
@@ -81,59 +63,9 @@ const config: Config = {
 					position: "left",
 					label: "Tutorial",
 				},
-				{ to: "/blog", label: "Blog", position: "left" },
-				{
-					href: "https://github.com/facebook/docusaurus",
-					label: "GitHub",
-					position: "right",
-				},
 			],
 		},
-		footer: {
-			style: "dark",
-			links: [
-				{
-					title: "Docs",
-					items: [
-						{
-							label: "Tutorial",
-							to: "/docs/intro",
-						},
-					],
-				},
-				{
-					title: "Community",
-					items: [
-						{
-							label: "Stack Overflow",
-							href: "https://stackoverflow.com/questions/tagged/docusaurus",
-						},
-						{
-							label: "Discord",
-							href: "https://discordapp.com/invite/docusaurus",
-						},
-						{
-							label: "Twitter",
-							href: "https://twitter.com/docusaurus",
-						},
-					],
-				},
-				{
-					title: "More",
-					items: [
-						{
-							label: "Blog",
-							to: "/blog",
-						},
-						{
-							label: "GitHub",
-							href: "https://github.com/facebook/docusaurus",
-						},
-					],
-				},
-			],
-			copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-		},
+
 		prism: {
 			//   theme: prismThemes.github,
 			//   darkTheme: prismThemes.dracula,
